03c2808dd7a97e465318047f6ee783527513a84d
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-27 17:09:39 +0200

    Initial CareGroup model

---
+++ project/care_groups/models.py
@@ -0,0 +1,20 @@
+import uuid
+from django.db import models
+from django.db.models.fields import CharField
+from django.utils.translation import gettext as _
+from django.urls import reverse
+
+
+class CareGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = _("care group")
+        verbose_name_plural = _("care groups")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("care-group-detail", kwargs={"pk": self.pk})
