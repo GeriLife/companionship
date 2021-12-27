175a38c5c3910a26f0591d426f67f49c0f1ef34c
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-27 17:32:39 +0200

    Add CareGroupListView URL

--- project/care_groups/urls.py
+++ project/care_groups/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CareGroupDetailView
+from .views import CareGroupDetailView, CareGroupListView
 
 urlpatterns = [
+    path('', CareGroupListView.as_view(), name='care-group-list'),
     path('<slug:pk>/', CareGroupDetailView.as_view(), name='care-group-detail'),
 ]
