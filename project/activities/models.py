29cea7187a6687fa1bbbb6484fb910f6b9a29288
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-28 16:23:30 +0200

    Initial Activity model

--- project/activities/models.py
+++ project/activities/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Activity(models.Model):
+    class ActivityTypeChoices(models.TextChoices):
+        APPOINTMENT = "APPOINTMENT", _("Appointment")
+        CALL = "CALL", _("Call")
+        ENTERTAINMENT = "ENTERTAINMENT", _("Entertainment")
+        ERRAND = "ERRAND", _("Errand")
+        HOUSEWORK = "HOUSEWORK", _("Housework")
+        NATURE = "NATURE", _("Nature")
+        OUTING = "OUTING", _("Outing")
+        SHOPPING = "SHOPPING", _("Shopping")
+
+    activity_type = models.CharField(
+        max_length=15,
+        choices=ActivityTypeChoices.choices,
+        default=ActivityTypeChoices.APPOINTMENT,
+    )
