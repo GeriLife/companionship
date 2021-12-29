3cdb31a1406fae64b70ad3e3e4814ee994970995
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-29 13:01:52 +0200

    Add ActivityCreateView

--- project/activities/views.py
+++ project/activities/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from .models import Activity
+
+
+class ActivityCreateView(CreateView):
+    model = Activity
+    fields = [
+        "care_group",
+        "activity_type",
+        "activity_date",
+    ]
+
+    def get_success_url(self):
+        return reverse("care-group-detail", kwargs={"pk": self.object.care_group.id })
