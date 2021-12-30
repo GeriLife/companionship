23fbdcfb605f2fc0a866d5399ef22d690ab1cede
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-30 09:52:11 +0200

    Add ActivityUpdateView

--- project/activities/urls.py
+++ project/activities/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
 from .views import (
-    ActivityCreateView
+    ActivityCreateView,
+    ActivityUpdateView,
 )
 
 urlpatterns = [
     path(
         "create",
         ActivityCreateView.as_view(),
+        name="activity-create",
+    ),
+    path(
+        "update",
+        ActivityUpdateView.as_view(),
         name="activity-create",
     ),
 ]
--- project/activities/views.py
+++ project/activities/views.py
@@ -1,10 +1,22 @@
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from .models import Activity
 
 
 class ActivityCreateView(CreateView):
+    model = Activity
+    fields = [
+        "care_group",
+        "activity_type",
+        "activity_date",
+    ]
+
+    def get_success_url(self):
+        return reverse("care-group-detail", kwargs={"pk": self.object.care_group.id })
+
+
+class ActivityUpdateView(UpdateView):
     model = Activity
     fields = [
         "care_group",
