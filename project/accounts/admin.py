658a1c844fb9d16bbfae7098494ffc6e8fd4a193
Brylie Christopher Oxley <brylie@amble.fi>
2022-01-04 20:40:55 +0200

    Remove username field and fix ordering/filtering

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -9,4 +9,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email', 'username',]
+    list_display = ['email',]
+    ordering = ['email',]
+    list_filter = []
