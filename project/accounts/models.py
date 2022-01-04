bb09fb5eb6458b03439dda660fdb9ed9fe876769
Brylie Christopher Oxley <brylie@amble.fi>
2022-01-04 13:09:20 +0200

    Add User.get_activity_count

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     
     def __str__(self):
         return self.username
+
+    def get_activity_count(self, care_group=None):
+        if care_group:
+            activity_count = self.activities.filter(
+                care_group=care_group
+            ).count
+        else:
+            activity_count = self.activities.count
+
+        return activity_count
