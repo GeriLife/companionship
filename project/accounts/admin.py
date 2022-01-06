23e30bba9e76cfa60df2f97c907da6724cbd6355
Brylie Christopher Oxley <brylie@amble.fi>
2022-01-06 20:00:53 +0200

    Fix CustomUserAdmin

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -4,11 +4,55 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email',]
-    ordering = ['email',]
-    list_filter = []
+    list_display = [
+        "email",
+        "display_name",
+    ]
+    ordering = [
+        "email",
+        "display_name",
+    ]
+    list_filter = [
+        "is_staff",
+        "is_superuser",
+    ]
+    fieldsets = (
+        (
+            "Details",
+            {
+                "fields": ("email", "display_name", "password"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "display_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "display_name")
