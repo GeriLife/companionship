00565b184ef455763e2a13fcbd996c5e5f655c57
Brylie Christopher Oxley <brylie@amble.fi>
2022-01-14 12:02:21 +0200

    Configure media settings

--- project/core/settings.py
+++ project/core/settings.py
@@ -147,6 +147,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
