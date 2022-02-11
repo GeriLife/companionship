d8c1af9ef83232eea39916e876e984ce58b142c8
Brylie Christopher Oxley <brylie@amble.fi>
2022-02-11 11:33:30 +0200

    Move verb to end of URL

--- project/people/urls.py
+++ project/people/urls.py
@@ -5,6 +5,7 @@ from .views import (
     PersonDetailView,
     PersonListView,
     PersonUpdateView,
+    join_as_companion,
 )
 
 urlpatterns = [
@@ -14,6 +15,7 @@ urlpatterns = [
         PersonCreateView.as_view(),
         name="person-create",
     ),
+    path("<slug:pk>/update/", PersonUpdateView.as_view(), name="person-update"),
     path("<slug:person_id>/join/", join_as_companion, name="person-join"),
     path("<slug:pk>/", PersonDetailView.as_view(), name="person-detail"),
 ]
