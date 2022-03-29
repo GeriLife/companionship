28f23475f38607c26699f0c814ed8603d2f1fce3
Brylie Christopher Oxley <brylie@amble.fi>
2022-03-29 15:40:18 +0300

    Initial view to set activity as done

--- project/activities/views.py
+++ project/activities/views.py
@@ -61,4 +61,33 @@ class ActivityRemoveParticipantView(View):
                 "person-detail",
                 kwargs={"pk": activity.person.id},
             )
+
+class ActivitySetDoneView(UserPassesTestMixin, View):
+
+    def test_func(self, *args, **kwargs):
+        """Only activity participants or Person's care organizers can update activity"""
+        self.activity = Activity.objects.get(id=self.kwargs["activity_id"])
+
+        user_is_participant = self.request.user in self.activity.participants.all()
+        user_is_organizer = self.request.user in self.activity.person.organizers
+
+        user_can_update_activity = (
+            user_is_participant or
+            user_is_organizer
+        )
+        
+        return user_can_update_activity
+
+    def get(self, request, activity_id, *args, **kwargs):
+        """If user passes permission tests, set activity as done."""
+
+        self.activity.done = True
+
+        self.activity.save()
+
+        return redirect(
+            reverse(
+                "person-detail",
+                kwargs={"pk": self.activity.person.id},
+            )
         )
