458a968dc185fb839599c8c775269f1393c6721e
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-11 22:25:38 +0300

    Initial CaregiverLocality model

--- project/caregivers/models.py
+++ project/caregivers/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 
+from django_countries.fields import CountryField
+
 
 class Caregiver(models.Model):
     class CaregiverType(models.TextChoices):
         INDIVIDUAL = "INDIVIDUAL", "Individual"
         ORGANIZATION = "ORGANIZATION", "Organization"
 
-
     display_name = models.CharField(max_length=255)
     type = models.CharField(
-        max_length=15,
-        choices=CaregiverType.choices,
-        default=CaregiverType.INDIVIDUAL
+        max_length=15, choices=CaregiverType.choices, default=CaregiverType.INDIVIDUAL
+    )
+
+
+class CaregiverLocality(models.Model):
+    """Represents the locality  (i.e. city) where a caregiver provides service."""
+
+    country = CountryField()
+    region = models.CharField(
+        max_length=255,
+        help_text="The region in which the locality is, and which is in the country. For example, California or another appropriate first-level Administrative division",
+    )
+    locality = models.CharField(
+        max_length=255,
+        help_text="The locality in which the street address is, and which is in the region. For example, Mountain View.",
     )
