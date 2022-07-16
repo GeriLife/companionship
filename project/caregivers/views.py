e894bccdacb80ac6e8b1a1a7d4fa27ff49e65d54
Brylie Christopher Oxley <brylie@amble.fi>
2022-07-16 14:48:52 +0300

    Initial CaregiverListView

--- project/caregivers/views.py
+++ project/caregivers/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Caregiver
+
+class CaregiverListView(ListView):
+    model = Caregiver
