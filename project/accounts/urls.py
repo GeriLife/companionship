d7e86f7276c9aa99df183ec8d3b1a6fded2d0177
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-30 23:20:04 +0300

    Remove commented code

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-# from .views import SignUpView, user_profile
 from .views import SignUpView, UserProfileUpdateView
 
 urlpatterns = [
-    # path("profile/", user_profile, name="user-profile"),
     path("profile/", UserProfileUpdateView.as_view(), name="user-profile"),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
