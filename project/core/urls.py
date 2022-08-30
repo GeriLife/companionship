1c53d9f7c3f19b60630ead1d8abd0d21dc3d7ce1
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-31 00:45:49 +0300

    Lint

--- project/core/urls.py
+++ project/core/urls.py
@@ -24,9 +24,9 @@ media_urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('activities/', include('activities.urls')),
-    path('caregivers/', include('caregivers.urls')),
-    path('people/', include('people.urls')),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("activities/", include("activities.urls")),
+    path("caregivers/", include("caregivers.urls")),
+    path("people/", include("people.urls")),
 ] + media_urlpatterns
