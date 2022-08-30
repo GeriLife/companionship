9efc9fca4ae5042a7cfa642b4ca8b663e64b7f83
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-31 00:39:44 +0300

    Lint

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, UserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,7 +11,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError(_("User must have an email address"))
-        
+
         email = self.normalize_email(email)
         user = self.model(email=email)
 
@@ -27,22 +31,25 @@ class CustomUserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_("email address"), unique=True)
     display_name = models.CharField(
-        _('display name'),
+        _("display name"),
         max_length=15,
-        help_text=_('Helps other team members recognise you, such as by your given name or nickname.'),
+        help_text=_(
+            "Helps other team members recognise you, such as by your given name or nickname."
+        ),
         default="Given name",
     )
     is_staff = models.BooleanField(
-        _('staff status'),
+        _("staff status"),
         default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
+        help_text=_("Designates whether the user can log into this admin site."),
     )
 
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
 
     objects = CustomUserManager()
 
@@ -51,9 +58,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_activity_count(self, person=None):
         if person:
-            activity_count = self.activities.filter(
-                person=person
-            ).count
+            activity_count = self.activities.filter(person=person).count
         else:
             activity_count = self.activities.count
 
