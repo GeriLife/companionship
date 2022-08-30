00211d4c0edc1affe72cf85f4958a5b68edb5c38
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-30 23:58:59 +0300

    Use updateview with request user instance

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,10 +1,8 @@
-from django.contrib import messages
-from django.contrib.auth import get_user_model, login
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic.edit import CreateView, FormView
+from django.views.generic.edit import CreateView, UpdateView
 
 from .forms import CustomUserCreationForm, UpdateUserForm
 
@@ -23,19 +21,10 @@ class SignUpView(CreateView):
         return redirect(self.success_url)
 
 
-class UserProfileUpdateView(FormView):
+class UserProfileUpdateView(UpdateView):
     form_class = UpdateUserForm
     template_name = "accounts/profile.html"
     success_url = reverse_lazy("user-profile")
 
-    def get_initial(self):
-        initial = super().get_initial()
-        initial["display_name"] = self.request.user.display_name
-        initial["email"] = self.request.user.email
-        return initial
-
-    def form_valid(self, form):
-        get_user_model().objects.filter(id=self.request.user.id).update(
-            **form.cleaned_data
-        )
-        return super().form_valid(form)
+    def get_object(self):
+        return self.request.user
