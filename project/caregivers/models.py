1b590748afcdebb74c75bd82bfb356d787b9c339
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-31 00:45:10 +0300

    Add i18n

--- project/caregivers/models.py
+++ project/caregivers/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Caregiver(models.Model):
     class CaregiverType(models.TextChoices):
-        INDIVIDUAL = "INDIVIDUAL", "Individual"
-        ORGANIZATION = "ORGANIZATION", "Organization"
+        INDIVIDUAL = "INDIVIDUAL", _("Individual")
+        ORGANIZATION = "ORGANIZATION", _("Organization")
 
     display_name = models.CharField(max_length=255)
     type = models.CharField(
