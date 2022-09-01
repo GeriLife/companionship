70aedc9779625f316f261330efb5fcf045c695c4
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-01 21:57:17 +0300

    Add support for next on SignUpView

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -12,13 +12,30 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        next = self.request.GET.get("next")
+
+        if next:
+            context["next"] = next
+
+        return context
+
     def form_valid(self, form):
         """If the form is valid, save the associated model and log the user in."""
         user = form.save()
 
+        # Ensure the user is logged in
         login(self.request, user)
 
-        return redirect(self.success_url)
+        next = self.request.POST.get("next")
+
+        # Allow specifying the next URL when signing up
+        if next is not None:
+            return redirect(next)
+        else:
+            return redirect(self.success_url)
 
 
 class UserProfileUpdateView(UpdateView):
