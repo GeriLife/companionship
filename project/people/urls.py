5f64208612fc38e8b08d7d2575b06159ed1ca473
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-01 22:12:37 +0300

    Run isort again

--- project/people/urls.py
+++ project/people/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 
-from .views import (
-    CompanionDeleteView,
-    JoinRequestUpdateView,
-    PersonCreateView,
-    PersonDetailView,
-    PersonListView,
-    PersonUpdateView,
-    join_as_companion,
-)
+from .views import (CompanionDeleteView, JoinRequestUpdateView,
+                    PersonCreateView, PersonDetailView, PersonListView,
+                    PersonUpdateView, join_as_companion)
 
 urlpatterns = [
     path("", PersonListView.as_view(), name="person-list"),
