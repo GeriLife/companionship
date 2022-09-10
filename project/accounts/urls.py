ae111ce5fcd9302250981f6487c7a370b702d056
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-10 19:29:59 +0300

    Add reset password URLs

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,8 +1,51 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from .views import SignUpView, UserProfileUpdateView
 
 urlpatterns = [
     path("profile/", UserProfileUpdateView.as_view(), name="user-profile"),
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path(
+        "signup/",
+        SignUpView.as_view(template_name="accounts/signup.html"),
+        name="signup",
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset_form.html",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
