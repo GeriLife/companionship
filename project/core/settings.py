74517fd73d5466c1f826fe1776d7e8cb30cd3e26
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-11 20:29:21 +0300

    Use matching environment variables

--- project/core/settings.py
+++ project/core/settings.py
@@ -43,9 +43,9 @@ DJANGO_USE_SMTP_SERVER = env.bool("DJANGO_USE_SMTP_SERVER", False)
 if DJANGO_USE_SMTP_SERVER:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-    EMAIL_HOST = env.str("DJANGO_EMAIL_HOST")
-    EMAIL_PORT = env.int("DJANGO_EMAIL_PORT", 587)
-    EMAIL_HOST_USER = env.str("DJANGO_EMAIL_HOST_USER")
+    EMAIL_HOST = env.str("EMAIL_HOST")
+    EMAIL_PORT = env.int("EMAIL_PORT", 587)
+    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
 else:
