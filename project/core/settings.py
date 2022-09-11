76bb113c6162e2fd5cc2a233f98eb71816376785
Brylie Christopher Oxley <brylie@amble.fi>
2022-09-11 20:27:01 +0300

    Set default email port

--- project/core/settings.py
+++ project/core/settings.py
@@ -44,7 +44,7 @@ if DJANGO_USE_SMTP_SERVER:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
     EMAIL_HOST = env.str("DJANGO_EMAIL_HOST")
-    EMAIL_PORT = env.int("DJANGO_EMAIL_PORT")
+    EMAIL_PORT = env.int("DJANGO_EMAIL_PORT", 587)
     EMAIL_HOST_USER = env.str("DJANGO_EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
