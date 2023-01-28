65d5852d6cae7016b7bc1ea0215bef80ebddb4c6
Brylie Christopher Oxley <brylie@amble.fi>
2023-01-28 18:09:55 +0200

    Add localhost for development

--- project/core/settings.py
+++ project/core/settings.py
@@ -33,7 +33,13 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1", "testserver"])
+default_allowed_hosts = [
+    "127.0.0.1",
+    "testserver",
+    "localhost",
+]
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=default_allowed_hosts)
 
 CSRF_TRUSTED_ORIGINS = env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=[])
 
