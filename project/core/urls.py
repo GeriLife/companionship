b83778e8c49b7fa25b0f8b244d849ef65d4370d8
Brylie Christopher Oxley <brylie@amble.fi>
2023-02-08 16:57:55 +0200

    Run isort

--- project/core/urls.py
+++ project/core/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from accounts.views import ApiVerifyEmailView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
-
-from accounts.views import ApiVerifyEmailView
 
 media_urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
