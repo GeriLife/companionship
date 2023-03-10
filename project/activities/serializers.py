d1e5db2495023911c5a7ee907d762f03236e4e95
Bas Ernst <50556177+ErnstBas@users.noreply.github.com>
2023-03-10 06:51:35 +0100

    Update serializers.py
    deepsource issues fixed
    
    Signed-off-by: Bas Ernst <50556177+ErnstBas@users.noreply.github.com>

--- project/activities/serializers.py
+++ project/activities/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Activity
 
+
 class ActivitySerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Activities
+    """
     class Meta:
         model = Activity
         fields = '__all__'
